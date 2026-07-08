cbfa691e3f51ad8592a7f6c25ac499112e18f3d0
水灵 <1668589925@qq.com>
2026-07-08 18:38:45 +0800

    style: 统一代码缩进和字符串引号风格
    调整所有代码的缩进为2空格，将单引号替换为双引号，统一代码格式风格

--- _worker.js
+++ _worker.js
@@ -1,113 +1,113 @@
-import { connect } from 'cloudflare:sockets';
+import { connect } from "cloudflare:sockets";
 
 export default {
-	async fetch(访问请求) {
-		const 读取我的请求标头 = 访问请求.headers.get('Upgrade');
-		const url = new URL(访问请求.url);
-		if (读取我的请求标头 === 'websocket') {
-			const 反代IP = url.searchParams.get('ip') || '';
-			return await 升级WS请求(反代IP);
-		}
-		return new Response('Not found', { status: 404 });
-	},
+  async fetch(访问请求) {
+    const 读取我的请求标头 = 访问请求.headers.get("Upgrade");
+    const url = new URL(访问请求.url);
+    if (读取我的请求标头 === "websocket") {
+      const 反代IP = url.searchParams.get("ip") || "";
+      return await 升级WS请求(反代IP);
+    }
+    return new Response("Not found", { status: 404 });
+  },
 };
 
 async function 升级WS请求(反代IP) {
-	const [客户端, WS接口] = Object.values(new WebSocketPair());
-	WS接口.accept();
-	WS接口.binaryType = 'arraybuffer';
-	WS接口.send(new Uint8Array([0, 0]));
-	启动传输管道(WS接口, 反代IP);
-	return new Response(null, { status: 101, webSocket: 客户端 });
+  const [客户端, WS接口] = Object.values(new WebSocketPair());
+  WS接口.accept();
+  WS接口.binaryType = "arraybuffer";
+  WS接口.send(new Uint8Array([0, 0]));
+  启动传输管道(WS接口, 反代IP);
+  return new Response(null, { status: 101, webSocket: 客户端 });
 }
 
 async function 启动传输管道(WS接口, 反代IP) {
-	let TCP接口;
-	let 传输数据;
+  let TCP接口;
+  let 传输数据;
 
-	const stream = new ReadableStream({
-		start(controller) {
-			WS接口.addEventListener('message', (event) => {
-				controller.enqueue(event.data);
-			});
-		},
-	});
+  const stream = new ReadableStream({
+    start(controller) {
+      WS接口.addEventListener("message", (event) => {
+        controller.enqueue(event.data);
+      });
+    },
+  });
 
-	await stream.pipeTo(
-		new WritableStream({
-			async write(chunk) {
-				if (传输数据) {
-					await 传输数据.write(chunk);
-				} else {
-					await 解析VL标头(chunk);
-				}
-			},
-		}),
-	);
+  await stream.pipeTo(
+    new WritableStream({
+      async write(chunk) {
+        if (传输数据) {
+          await 传输数据.write(chunk);
+        } else {
+          await 解析VL标头(chunk);
+        }
+      },
+    }),
+  );
 
-	async function 解析VL标头(VL数据) {
-		const 获取数据定位 = new Uint8Array(VL数据)[17];
-		const 提取端口索引 = 18 + 获取数据定位 + 1;
-		const 建立端口缓存 = VL数据.slice(提取端口索引, 提取端口索引 + 2);
-		const 访问端口 = new DataView(建立端口缓存).getUint16(0);
-		const 提取地址索引 = 提取端口索引 + 2;
-		const 建立地址缓存 = new Uint8Array(VL数据.slice(提取地址索引, 提取地址索引 + 1));
-		const 识别地址类型 = 建立地址缓存[0];
-		let 地址长度 = 0;
-		let 访问地址 = '';
-		let 地址信息索引 = 提取地址索引 + 1;
+  async function 解析VL标头(VL数据) {
+    const 获取数据定位 = new Uint8Array(VL数据)[17];
+    const 提取端口索引 = 18 + 获取数据定位 + 1;
+    const 建立端口缓存 = VL数据.slice(提取端口索引, 提取端口索引 + 2);
+    const 访问端口 = new DataView(建立端口缓存).getUint16(0);
+    const 提取地址索引 = 提取端口索引 + 2;
+    const 建立地址缓存 = new Uint8Array(VL数据.slice(提取地址索引, 提取地址索引 + 1));
+    const 识别地址类型 = 建立地址缓存[0];
+    let 地址长度 = 0;
+    let 访问地址 = "";
+    let 地址信息索引 = 提取地址索引 + 1;
 
-		switch (识别地址类型) {
-			case 1:
-				地址长度 = 4;
-				访问地址 = new Uint8Array(VL数据.slice(地址信息索引, 地址信息索引 + 地址长度)).join('.');
-				break;
-			case 2:
-				地址长度 = new Uint8Array(VL数据.slice(地址信息索引, 地址信息索引 + 1))[0];
-				地址信息索引 += 1;
-				访问地址 = new TextDecoder().decode(VL数据.slice(地址信息索引, 地址信息索引 + 地址长度));
-				break;
-			case 3:
-				地址长度 = 16;
-				const dataView = new DataView(VL数据.slice(地址信息索引, 地址信息索引 + 地址长度));
-				const ipv6 = [];
-				for (let i = 0; i < 8; i++) {
-					ipv6.push(
-						dataView
-							.getUint16(i * 2)
-							.toString(16)
-							.padStart(4, '0'),
-					);
-				}
-				访问地址 = ipv6.join(':');
-				break;
-			default:
-				return;
-		}
+    switch (识别地址类型) {
+      case 1:
+        地址长度 = 4;
+        访问地址 = new Uint8Array(VL数据.slice(地址信息索引, 地址信息索引 + 地址长度)).join(".");
+        break;
+      case 2:
+        地址长度 = new Uint8Array(VL数据.slice(地址信息索引, 地址信息索引 + 1))[0];
+        地址信息索引 += 1;
+        访问地址 = new TextDecoder().decode(VL数据.slice(地址信息索引, 地址信息索引 + 地址长度));
+        break;
+      case 3:
+        地址长度 = 16;
+        const dataView = new DataView(VL数据.slice(地址信息索引, 地址信息索引 + 地址长度));
+        const ipv6 = [];
+        for (let i = 0; i < 8; i++) {
+          ipv6.push(
+            dataView
+              .getUint16(i * 2)
+              .toString(16)
+              .padStart(4, "0"),
+          );
+        }
+        访问地址 = ipv6.join(":");
+        break;
+      default:
+        return;
+    }
 
-		const 写入初始数据 = VL数据.slice(地址信息索引 + 地址长度);
+    const 写入初始数据 = VL数据.slice(地址信息索引 + 地址长度);
 
-		try {
-			TCP接口 = connect({ hostname: 访问地址, port: 访问端口 });
-			await TCP接口.opened;
-		} catch {
-			const [反代IP地址, 反代IP端口 = 访问端口] = 反代IP.split(':');
-			TCP接口 = connect({ hostname: 反代IP地址, port: Number(反代IP端口) || 访问端口 });
-			await TCP接口.opened;
-		}
+    try {
+      TCP接口 = connect({ hostname: 访问地址, port: 访问端口 });
+      await TCP接口.opened;
+    } catch {
+      const [反代IP地址, 反代IP端口 = 访问端口] = 反代IP.split(":");
+      TCP接口 = connect({ hostname: 反代IP地址, port: Number(反代IP端口) || 访问端口 });
+      await TCP接口.opened;
+    }
 
-		传输数据 = TCP接口.writable.getWriter();
+    传输数据 = TCP接口.writable.getWriter();
 
-		if (写入初始数据?.byteLength > 0) {
-			await 传输数据.write(写入初始数据);
-		}
+    if (写入初始数据?.byteLength > 0) {
+      await 传输数据.write(写入初始数据);
+    }
 
-		TCP接口.readable.pipeTo(
-			new WritableStream({
-				write(chunk) {
-					WS接口.send(chunk);
-				},
-			}),
-		);
-	}
+    TCP接口.readable.pipeTo(
+      new WritableStream({
+        write(chunk) {
+          WS接口.send(chunk);
+        },
+      }),
+    );
+  }
 }
